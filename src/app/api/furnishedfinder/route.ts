979a1b1c2cd198349263ab64df42c09665b47f78
Adil0710 <padil2246@gmail.com>
2025-03-06 18:31:58 +0530

    Added FurnishedFinder API

--- src/app/api/furnishedfinder/route.ts
+++ src/app/api/furnishedfinder/route.ts
@@ -41,8 +41,8 @@ export async function POST(request: Request) {
       searchCoordinates: [],
       searchArea: "0.25",
       maxItems: 20,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any @typescript-eslint/no-unused-vars
-      extendOutputFunction: ($: any) => ({}),
+
+      extendOutputFunction: (_$: unknown) => ({}),
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       customMapFunction: (object: any) => ({ ...object }),
       proxy: {
